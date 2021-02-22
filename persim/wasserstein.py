53c8677003b937323faad951e7f00e322cf4c608
Talha Bin Masood <talha.bin.masood@liu.se>
2021-02-22 16:59:21 +0100

    Make Wasserstein distance more flexible, similar to gudhi

--- persim/wasserstein.py
+++ persim/wasserstein.py
@@ -1,46 +1,38 @@
 """
-
     Implementation of the Wasserstein distance using
     the Hungarian algorithm
-
     Author: Chris Tralie
-
 """
 import numpy as np
 from sklearn import metrics
 from scipy import optimize
 import warnings
+import math
 
 __all__ = ["wasserstein"]
 
 
-def wasserstein(dgm1, dgm2, matching=False):
+def wasserstein(dgm1, dgm2, order=1.0, internal_p=math.inf, matching=False):
     """
     Perform the Wasserstein distance matching between persistence diagrams.
     Assumes first two columns of dgm1 and dgm2 are the coordinates of the persistence
     points, but allows for other coordinate columns (which are ignored in
     diagonal matching).
-
     See the `distances` notebook for an example of how to use this.
-
     Parameters
     ------------
-
     dgm1: Mx(>=2) 
         array of birth/death pairs for PD 1
     dgm2: Nx(>=2) 
         array of birth/death paris for PD 2
     matching: bool, default False
         if True, return matching information and cross-similarity matrix
-
     Returns 
     ---------
-
     d: float
         Wasserstein distance between dgm1 and dgm2
     (matching, D): Only returns if `matching=True`
         (tuples of matched indices, (N+M)x(N+M) cross-similarity matrix)
-
     """
 
     S = np.array(dgm1)
@@ -70,9 +62,18 @@ def wasserstein(dgm1, dgm2, matching=False):
     if N == 0:
         T = np.array([[0, 0]])
         N = 1
+    
     # Step 1: Compute CSM between S and dgm2, including points on diagonal
-    DUL = metrics.pairwise.pairwise_distances(S, T)
 
+    if internal_p == 1.0:
+        DUL = metrics.pairwise.pairwise_distances(S, T, metric = "l1")
+    elif internal_p == 2.0:
+        DUL = metrics.pairwise.pairwise_distances(S, T)
+    elif internal_p == math.inf:
+        DUL = metrics.pairwise.pairwise_distances(S, T, metric = "chebyshev") 
+    else:
+        DUL = metrics.pairwise.pairwise_distances(S, T, metric = "minkowski", p=internal_p) 
+    
     # Put diagonal elements into the matrix
     # Rotate the diagrams to make it easy to find the straight line
     # distance to the diagonal
@@ -92,7 +93,12 @@ def wasserstein(dgm1, dgm2, matching=False):
 
     # Step 2: Run the hungarian algorithm
     matchi, matchj = optimize.linear_sum_assignment(D)
-    matchdist = np.sum(D[matchi, matchj])
+    if order == 1.0:
+        matchdist = np.sum(D[matchi, matchj])
+    elif order == math.inf:
+        matchdist = np.max(D[matchi, matchj])
+    else:
+        matchdist = np.sum(D[matchi, matchj]**order)**(1.0/order)
 
     if matching:
         matchidx = [(i, j) for i, j in zip(matchi, matchj)]
